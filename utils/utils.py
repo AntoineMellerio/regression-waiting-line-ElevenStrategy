0a915f5d63dc87ccc6af64af4f493ef8a5aaeeaf
abhishikth10 <123946072+abhishikth10@users.noreply.github.com>
2023-02-01 00:55:35 +0100

    Update utils.py
    Add function to generate heatmap for detailed insights

--- utils/utils.py
+++ utils/utils.py
@@ -11,7 +11,7 @@ def attendance_figures(df, attraction_list, start_date, end_date, date_label, at
         start_date : str = starting date of the period with format yyyy/mm/dd.
         end_date : str = ending date of the period with format yyyy/mm/dd.
         date_label : str = label of the date column.
-        attendance_label : str = mabel of the attendance column.
+        attendance_label : str = label of the attendance column.
         attraction_label : str = label of the attraction column.
 
     Outputs:
@@ -43,3 +43,60 @@ def attendance_figures(df, attraction_list, start_date, end_date, date_label, at
     fig.update_layout(yaxis_title='Daily Attendance', width=800, height=400)
     
     return fig, [val_min, val_avg, val_max]
+
+def wait_time_figures(df, attraction_list, start_date, end_date, date_label, wait_time_label, attraction_label):
+    '''
+    Return the heatmap for wait times per ride on a daily basis. 
+    Inputs:
+        df : pd.DataFrame() = table of the wait time per ride every 15 min and an assortment of other related features. 
+        attraction_list : list(str) = list of the names of the attractions.
+        start_date : str = starting date of the period with format yyyy/mm/dd.
+        end_date : str = ending date of the period with format yyyy/mm/dd.
+        date_label : str = label of the date column.
+        attendance_label : str = label of the attendance column.
+        attraction_label : str = label of the attraction column.
+    Outputs:
+       fig: A detailed heatmap that shows the variation of wait times per day for selected rides in the given timeframe
+    '''
+    to_plot = df.copy()
+
+    # Change date types
+    to_plot[f"{date_label}"] = pd.to_datetime(to_plot[f"{date_label}"])
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
+
+    # Replace negative values with zero values
+    to_plot.loc[to_plot[f"{wait_time_label}"]<0, f"{wait_time_label}"] = 0
+
+    # Filter the table
+    to_plot = to_plot[(to_plot[f"{date_label}"]>=start_date)&(to_plot[f"{date_label}"]<=end_date)][to_plot[f"{attraction_label}"].isin(attraction_list)]
+
+    # Create a dataframe dictionary with each ride name as key
+    unique_rides = to_plot[f"{attraction_label}"].unique()
+    df_dict = {elem : pd.DataFrame() for elem in unique_rides}
+    for key in df_dict.keys():
+        df_dict[key] = to_plot[:][to_plot[f"{attraction_label}"] == key]
+
+    # Group the table by date and extract arrays of wait times for each ride per day
+    namelist = unique_rides.tolist()
+    complete_list = []
+    for elem in namelist:
+        df = df_dict[elem]
+        rslt_df = df[[f"{date_label}", f"{wait_time_label}"]]
+        rslt_df = rslt_df.groupby([f"{date_label}"]).sum()
+        list = rslt_df[f"{wait_time_label}"].tolist()
+        complete_list.append(list)
+    
+    # Generate plot
+    fig = go.Figure(data=go.Heatmap(
+        z=complete_list,
+        x=rslt_df.index,
+        y=unique_rides,
+        colorscale='Viridis'))
+
+    fig.update_layout(
+        title='Cumulative Wait Times of Each Ride Per Day (in minutes)',
+        xaxis_nticks = len(rslt_df.index),
+        width=800, height=400)
+    
+    return fig
