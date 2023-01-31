35761c2eb67e8ed999029dcb3c6dae9c965afcfa
Antoine Mellerio <antoine.mellerio@orange.fr>
2023-01-31 16:48:50 +0100

    Add attendance display to the overview page

--- utils/utils.py
+++ utils/utils.py
@@ -1,46 +1,45 @@
-import tensorflow as tf
-import os
+import pandas as pd
+import plotly.graph_objects as go
 
-model = tf.keras.models.load_model(os.getcwd + 'utils/saved_model.tar.gz')
-
-def classif_silo(list_input_image_path, model):
+def attendance_figures(df, attraction_list, start_date, end_date, date_label, attendance_label, attraction_label):
     '''
-    Detects the probability that the pictures have a silo in them or not.
-    Args
-        list_input_image_path = list(str) : list of the path of the pictures (png)
-        model : trained tensorflow model
-    Returns
-        prediction = np.array(proba) : array of the probabilities that each picture contains a silo.
+    Return the attendance mean, max and min. 
+
+    Inputs:
+        df : pd.DataFrame() = table of the attendance per date and per attraction. 
+        attraction_list : list(str) = list of the names of the attractions.
+        start_date : str = starting date of the period with format yyyy/mm/dd.
+        end_date : str = ending date of the period with format yyyy/mm/dd.
+        date_label : str = label of the date column.
+        attendance_label : str = mabel of the attendance column.
+        attraction_label : str = label of the attraction column.
+
+    Outputs:
+        mean : int = average daily attendnace of the attraction over the period.
+        min : min daily attendance at the attraction during the period.
+        max : max daily attendance at the attraction during the perdiod. 
     '''
-    
-    def parse(input_image_path):
-        # Read an image from a file
-        image_string = tf.io.read_file(input_image_path)
-        # Decode it into a dense vector
-        image_decoded = tf.image.decode_jpeg(image_string, channels=CHANNELS)
-        # Resize it to fixed shape
-        image_resized = tf.image.resize(image_decoded, [IMG_SIZE, IMG_SIZE])
-        # Normalize it from [0, 255] to [0.0, 1.0]
-        image_normalized = image_resized / 255.0
+    to_plot = df.copy()
 
-        return (image_normalized)
-    
-    dataset = tf.data.Dataset.from_tensor_slices((list_input_image_path))
-    dataset = dataset.map(parse, num_parallel_calls=AUTOTUNE)
-    
-    # This is a small dataset, only load it once, and keep it in memory.
-    dataset = dataset.cache()
+    # Change date types
+    to_plot[f"{date_label}"] = pd.to_datetime(to_plot[f"{date_label}"])
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
 
-    # Batch the data for multiple steps
-    dataset = dataset.batch(BATCH_SIZE)
-    # Fetch batches in the background while the model is training.
-    dataset = dataset.prefetch(buffer_size=AUTOTUNE)
-    
-    prediction = model.predict(dataset)
-    
-    return prediction
+    # Replace negative values with zero values
+    to_plot.loc[to_plot[f"{attendance_label}"]<0, f"{attendance_label}"] = 0
 
-# Example of usage:
-#input_image_path1 = 'images_classified/not_silos/silos_256-0-0--6-14-1247-31272.png'
-#input_image_path2 = 'images_classified/not_silos/silos_256-0-0--6-14-1281-31369.png'
-#pred = classif_silo([input_image_path1, input_image_path2], model)
+    # Filter the table
+    to_plot = to_plot[(to_plot[f"{date_label}"]>start_date)&(to_plot[f"{date_label}"]<end_date)][to_plot[f"{attraction_label}"].isin(attraction_list)]
+
+    # Calculate figures
+    val_min = to_plot[f"{attendance_label}"].min()
+    val_avg = int(to_plot[f"{attendance_label}"].mean())
+    val_max = to_plot[f"{attendance_label}"].max()
+
+    # Generate plot
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=to_plot["USAGE_DATE"], y=to_plot["attendance"], mode="lines", name='Attendance', line=dict(color='#002244')))
+    fig.update_layout(yaxis_title='Daily Attendance', width=800, height=400)
+    
+    return fig, [val_min, val_avg, val_max]
